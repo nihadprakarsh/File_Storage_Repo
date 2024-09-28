3387831b243c989dcf52c46cc9701f3e0d97b93b
Nihad Prakarsh <nihadprakarsh@gmail.com>
2024-09-28 16:22:33 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import LandingPage from './components/LandingPage/landing'
+
+function App() {
+  return (
+    <>
+      <LandingPage></LandingPage>
+    </>
+  )
+}
+
+export default App
